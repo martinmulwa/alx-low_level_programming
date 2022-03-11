aad6e0b4210d6ee1a0543fc9abd0df395a6a3d6d
Martin Pius <martin.pius@students.jkuat.ac.ke>
2022-03-11 14:49:54 +0300

    add task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar('0' + i);
+
+	putchar('\n');
+
+	return (0);
+}

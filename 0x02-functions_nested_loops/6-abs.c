21dfb3f2cdf06395bb5bb1af5e646993ea233a22
Martin Pius <martin.pius@students.jkuat.ac.ke>
2022-03-15 14:10:36 +0300

    add task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: number to be computed
+ *
+ * Return: the absolute value of n
+ */
+int _abs(int n)
+{
+	return (n < 0 ? (n * -1) : n);
+}

6ef71ececb15c9d5b54fb29ac9c60e07c78f1e3f
Martin Pius <martin.pius@students.jkuat.ac.ke>
2022-03-16 15:42:46 +0300

    fix task 3 again

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,10 +11,10 @@
 */
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+	if (((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)))
 	{
 		/* check if it's past feb */
-		if (month >= 2 && day >= 60)
+		if (month > 2 && day >= 60)
 		{
 			day++;
 		}

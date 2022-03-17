61d7d3fa3e20f5a9acdfdf5ad096bc3973c316ed
Martin Pius <martin.pius@students.jkuat.ac.ke>
2022-03-17 19:46:03 +0300

    fix task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,16 +7,19 @@
  */
 void print_number(int n)
 {
-    /* print '-' in front of -ve integers and ensure n is +ve*/
+	unsigned int num = n;
+
+    /* print '-' in front of -ve integers and ensure n is +ve */
 	if (n < 0)
 	{
 		_putchar('-');
-		n *= -1;
+		num *= -1;
 	}
 
-	if (n >= 10)
-		print_number(n / 10);
+	/* if num has more than 1-digit, print preceding digit first */
+	if (num >= 10)
+		print_number(num / 10);
 
-	_putchar('0' + (n % 10));
+	_putchar('0' + (num % 10));
 
 }

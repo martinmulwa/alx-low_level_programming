302c15f6526c91355034a17fd424cec280ef6086
Martin Pius <martin.pius@students.jkuat.ac.ke>
2022-03-17 18:47:08 +0300

    add tests for 0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/tests/1-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char c;
+
+    c = 'a';
+    printf("%c: %d\n", c, _isdigit(c));
+    c = '9';
+    printf("%c: %d\n", c, _isdigit(c));
+    return (0);
+}

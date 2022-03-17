79e355724173ec98102afe9f82bacbdfa9641993
Martin Pius <martin.pius@students.jkuat.ac.ke>
2022-03-17 19:32:00 +0300

    fix task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -14,17 +14,9 @@ void print_number(int n)
 		n *= -1;
 	}
 
-	/* base case is when n is a 1-digit number*/
-	if (n < 10)
-	{
-		/* just print the number */
-		_putchar('0' + n);
-	}
-	/* if n has more than 1 digit */
-	else
-	{
+	if (n >= 10)
 		print_number(n / 10);
-		_putchar('0' + (n % 10));
-	}
+
+	_putchar('0' + (n % 10));
 
 }

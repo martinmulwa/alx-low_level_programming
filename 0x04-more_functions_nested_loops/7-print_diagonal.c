c0c7e94d2b2cb4d388e613d20f1e1f3049101b4f
Martin Pius <martin.pius@students.jkuat.ac.ke>
2022-03-17 15:06:20 +0300

    fix task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* print_line - draws a diagonal line of length n in the terminal
+* print_diagonal - draws a diagonal line of length n in the terminal
 * followed by a new line
 * @n: the number of times the character / should be printed
 *

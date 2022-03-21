974e2e90d8c5679882aa6bf54d7e4fcaf33b5459
Martin Pius <martin.pius@students.jkuat.ac.ke>
2022-03-21 17:27:45 +0300

    add task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array of integers,
+ * followed by a new line
+ * @a: address of the first ellement in the array
+ * @n: number of first lelements to print in the given array
+ *
+ * Description:
+ * Numbers must be separated by comma, followed by a space
+ * The numbers should be displayed in the same order
+ * as they are stored in the array
+ * assume the given n is less than the length of the array
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i < n - 1)
+			printf("%d, ", *(a + i));
+		else
+			printf("%d\n", *(a + i));
+	}
+}

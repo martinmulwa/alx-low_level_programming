6ad300beec9c050944fea18c3841028eaeb1f8e2
Martin Pius <martin.pius@students.jkuat.ac.ke>
2022-03-21 18:03:58 +0300

    fix task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strcpy - makes a copy of a string
@@ -9,9 +10,15 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	do {
-		*dest++ = *src++;
-	} while (*src);
+	int i = 0;
+
+	while (*(src + i))
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+
+	*(dest + i) = '\0';
 
 	return (dest);
 }

467dd61e291a5e23d2993bc34d45f862f78fc969
Martin Pius <martin.pius@students.jkuat.ac.ke>
2022-03-21 14:32:22 +0300

    add task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - finds the length of a string
+ * @s: address of first character in the string
+ *
+ * Return: length og the string
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s)
+	{
+		length++;
+		s++;
+	}
+
+	return (length);
+}

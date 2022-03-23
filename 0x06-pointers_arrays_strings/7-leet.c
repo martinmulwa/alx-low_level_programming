84c7e1410b25d7fb32324ae93f4faf09394c544c
Martin Pius <martin.pius@students.jkuat.ac.ke>
2022-03-23 18:58:27 +0300

    fix task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,19 +9,21 @@
 char *leet(char *str)
 {
 	/* conversion tables */
-	char letters[] = {'a', 'e', 'o', 't', 'l'};
-	int values[] = {4, 3, 0, 7, 1};
+	char letters[] = "aeotl";
+	char values[] = "43071";
 
 	int i;
 	int j;
-	int n = sizeof(letters) / sizeof(letters[0]);
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		for (j = 0; j < n; j++)
+		for (j = 0; letters[j] != '\0'; j++)
 		{
 			if (str[i] == letters[j] || str[i] == letters[j] - 32)
-				str[i] = values[j] + '0';
+			{
+				str[i] = values[j];
+				break;
+			}
 		}
 	}
 

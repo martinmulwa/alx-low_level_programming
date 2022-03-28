75c58948cc5d73834b8c48b2cfa66eb81985a327
Martin Pius <martin.pius@students.jkuat.ac.ke>
2022-03-28 21:05:45 +0300

    fix task 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -15,8 +15,8 @@ void print_diagsums(int *a, int size)
 
 	for (i = 0; i < size; i++)
 	{
-		sum1 += a[i * (size + 1)];
-		sum2 += a[(i + 1) * (size - 1)];
+		sum1 += a[(size * i) + i];
+		sum2 += a[(size * (i + 1)) - (i + 1)];
 	}
 
 	printf("%u, %u\n", sum1, sum2);

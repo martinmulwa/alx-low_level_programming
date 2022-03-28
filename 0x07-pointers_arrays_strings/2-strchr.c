9142424d95c088b5d7c551cb0e80629cef47fdf6
Martin Pius <martin.pius@students.jkuat.ac.ke>
2022-03-28 17:18:50 +0300

    fix task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,20 +8,18 @@
  *
  * Return: pointer to the first occurence of c in s. Otherwise NULL
  */
-char *_strchr(char *s, char c)
-{
-	int a;
+ char *_strchr(char *s, char c)
+ {
+ 	int i;
 
-	while (1)
-	{
-		a = *s++;
-		if (a == c)
-		{
-			return (s - 1);
-		}
-		if (a == 0)
-		{
-			return (NULL);
-		}
-	}
-}
+ 	for (i = 0; s[i] != '\0'; i++)
+ 	{
+ 		if (c == s[i])
+ 			return (s + i);
+ 	}
+
+	if (c == s[i])
+		return (s + i);
+
+ 	return (NULL);
+ }

95de2aebefcaedaa8a2ab4687914781813202790
Martin Pius <martin.pius@students.jkuat.ac.ke>
2022-03-30 17:56:57 +0300

    add task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - prints the phrase "Programming is like building a multilingual puzzle
- * Return: 0 if exited properly, non-zero otherwise
+ * main - prints exactly "Programming is like building a multilingual puzzle,
+ * followed by a new line
+ *
+ * Return: 0 always
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }

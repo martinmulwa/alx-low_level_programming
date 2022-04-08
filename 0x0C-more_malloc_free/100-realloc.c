e0ca888f5e2b1ecd3b6250c76e06885ce2abc529
Martin Pius <martin.pius@students.jkuat.ac.ke>
2022-04-08 13:42:11 +0300

    fix task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -26,6 +26,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int lim = (old_size < new_size) ? old_size : new_size;
 	unsigned int i;
 
+	/* If new_size == old_size do not do anything and return ptr */
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
@@ -42,7 +43,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 
 	/* copy all the contents from ptr to the new memory */
-	for (i = 0; i < lim; i++)
+	for (i = 0; i < lim && ptr != NULL; i++)
 		mem[i] = ptr_cpy[i];
 
 	/* free the previous memory */

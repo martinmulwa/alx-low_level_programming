64ce44df1fd44d992fd521a045693e4614c60c80
Martin Pius <martin.pius@students.jkuat.ac.ke>
2022-04-10 11:29:44 +0300

    fix task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -6,11 +6,8 @@ void print_str(char *str);
 void print_num(char *str);
 int is_valid_int(char *str);
 int check_usage(int argc, char **argv);
-unsigned int _strlen(char *s);
-char *mult_str_c(char *str, char c);
-char *add_str(char *s1, char *s2);
-char *mult_str_n(char *str, int n);
 void error_exit(void);
+unsigned int _strlen(char *s);
 char *create_str(unsigned int len);
 
 /**
@@ -30,8 +27,9 @@ char *create_str(unsigned int len);
  */
 int main(int argc, char **argv)
 {
-	char *num1, *num2, *result, *tmp_result1, *tmp_result2;
-	unsigned int len1, len2, len, i, j;
+	char *num1, *num2, *result;
+	unsigned int len1, len2, len, i, j, k;
+	int dig1, dig2, dig, carry, tmp;
 
 	/* check correct usage */
 	if (!check_usage(argc, argv))
@@ -39,9 +37,9 @@ int main(int argc, char **argv)
 
 	num1 = argv[1];
 	num2 = argv[2];
-	len1 = _strlen(argv[1]);
-	len2 = _strlen(argv[2]);
-	len = len1 + len2 + 1;
+	len1 = _strlen(num1);
+	len2 = _strlen(num2);
+	len = len1 + len2;
 
 	/* allocate space for result */
 	result = create_str(len);
@@ -49,21 +47,22 @@ int main(int argc, char **argv)
 		error_exit();
 
 	/* multiplication */
-	for (i = len2, j = 0; i >= 1; i--, j++)
+	for (i = 1; i <= len1; i++)
 	{
-		/* multiply the whole of num1 with the current digit of num2 */
-		tmp_result1 = mult_str_c(num1, num2[i - 1]);
+		carry = 0;
+		k = i;
+		for (j = 1; j <= len2; j++, k++)
+		{
+			dig1 = num1[len1 - i] - '0';
+			dig2 = num2[len2 - j] - '0';
+			dig =  result[len - k] - '0';
 
-		/* multiply that result with 10 ^ j */
-		tmp_result2 = mult_str_n(tmp_result1, j);
-		free(tmp_result1);
-
-		/* add that result to the current result */
-		tmp_result1 = add_str(result, tmp_result2);
-		free(tmp_result2);
-		free(result);
-
-		result = tmp_result1;
+			tmp = (dig1 * dig2) + carry;
+			carry = tmp / 10;
+			result[len - k] = ((dig + (tmp % 10)) % 10) + '0';
+			carry += ((dig + (tmp % 10)) / 10);
+		}
+		result[len - k] = carry + '0';
 	}
 
 	print_num(result);
@@ -193,118 +192,7 @@ int check_usage(int argc, char **argv)
 	return (1);
 }
 
-/**
- * mult_str_c - multiplies an integer in a string
- * with a single digit as a character
- * @str: string representing integer
- * @c: character representing digit
- *
- * Return: string containing result
- */
-char *mult_str_c(char *str, char c)
-{
-	unsigned int i;
-	int carry = 0;
-	char *result;
-	unsigned int len = _strlen(str);
-	int a, b, tmp;
 
-	result = create_str(len + 1);
-	if (result == NULL)
-		return (NULL);
-
-	for (i = len; i >= 1; i--)
-	{
-		a = c - '0';
-		b = str[i - 1] - '0';
-
-		tmp = a * b + carry;
-
-		result[i] = (tmp % 10) + '0';
-		carry = tmp / 10;
-	}
-
-	if (carry)
-		result[i] = carry + '0';
-
-	return (result);
-}
-
-/**
- * mult_str_n - multiplies an integer represented as a strings
- * with a given multiple of 10
- * @str: string representing integer
- * @n: integer representing multiple of 10
- *
- * Return: string containing result
- */
-char *mult_str_n(char *str, int n)
-{
-	unsigned int len = _strlen(str);
-	unsigned int i;
-	char *result;
-
-	/* allocate space for result */
-	result = malloc(sizeof(char) * (len + n + 1));
-	if (result == NULL)
-		return (NULL);
-
-	/* copy all the content of str into result */
-	for (i = 0; i < len; i++)
-		result[i] = str[i];
-
-	/* write n zeroes from the end of str */
-	for (; i < len + n; i++)
-		result[i] = '0';
-
-	result[i] = '\0';
-
-	return (result);
-}
-
-/**
- * add_str - adds two strings that represent integers
- * @s1: 1st string
- * @s2: 2nd string
- *
- * Return: pointer to the result string
- */
-char *add_str(char *s1, char *s2)
-{
-	unsigned int len1, len2, len3;
-	unsigned int i, j, k;
-	int a, b, c, d, carry;
-	char *s3;
-
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
-	len3 = (len1 >= len2 ? len1 : len2) + 1;
-
-	/* allocate space for the result */
-	s3 = create_str(len3);
-	if (s3 == NULL)
-		return (NULL);
-
-	/* addition */
-	for (i = len1, j = len2, k = len3, carry = 0; i >= 1 || j >= 1; k--)
-	{
-		a = (i >= 1) ? s1[i - 1] - '0' : 0;
-		b = (j >= 1) ? s2[j - 1] - '0' : 0;
-		c = s3[k - 1] - '0';
-		d = a + b + c + carry;
-
-		s3[k - 1] = (d % 10) + '0';
-		carry = d / 10;
-
-		i = (i >= 1) ? i - 1 : i;
-		j = (j >= 1) ? j - 1 : j;
-	}
-
-	if (carry)
-		s3[k - 1] = carry + '0';
-
-	return (s3);
-}
 
 /**
  * _strlen - finds the length of a string

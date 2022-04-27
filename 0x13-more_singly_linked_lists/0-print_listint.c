86140776fc92c44766dee6aeaeed8a5e67cdf15b
Martin Pius <martin.pius@students.jkuat.ac.ke>
2022-04-27 18:44:05 +0300

    add task 12

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,8 +10,12 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count;
 
-	for (count = 0; h; h = h->next, count++)
+	while (h != NULL)
+	{
 		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
 
 	return (count);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -18,6 +18,20 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/**
+ * struct listnode_s - singly linked list
+ * @ptr: address of listint_t
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
+typedef struct listnode_s
+{
+	listint_t *ptr;
+	struct listnode_s *next;
+} listnode_t;
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -30,5 +44,6 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
 
 #endif

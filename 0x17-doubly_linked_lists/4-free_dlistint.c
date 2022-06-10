ba73626f5f004f399cfc50b3c66c557f0784dd36
Martin Pius <martin.pius@students.jkuat.ac.ke>
2022-06-10 16:40:32 +0300

    add task 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a dlistint_t list
+ * @head: pointer to the first node in the list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+
+		head = tmp;
+	}
+}
